a938bc3f67881c5af18a60a6f9d87c45e0339ff7
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-05-01 14:25:34 +0700

    learn inheritance

---
+++ src/main/java/com/hiwijaya/javabasic/Inheritance.java
@@ -0,0 +1,94 @@
+package com.hiwijaya.javabasic;
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        Tesla teslaModel3 = new Tesla();
+        teslaModel3.setTransmission("automatic");
+        teslaModel3.setFuel("electricity");
+        teslaModel3.drive();
+        teslaModel3.honk();
+        teslaModel3.setSelfDriving(true);
+    }
+
+}
+
+
+class Car {
+
+    private String transmission;
+    private String fuel;
+    private boolean parkingCam;
+    private int productionYear;
+
+    public Car(){
+        System.out.println("This is general car.");
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public boolean hasParkingCam() {
+        return parkingCam;
+    }
+
+    public void setParkingCam(boolean parkingCam) {
+        this.parkingCam = parkingCam;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public void drive(){
+        System.out.println("Drive");
+    }
+
+    public void honk() {
+        System.out.println("Telolet!");
+    }
+
+}
+
+class Tesla extends Car {
+
+    private boolean selfDriving;
+
+    public Tesla(){
+        super();
+        System.out.println("This is Tesla!");
+    }
+
+    public void setSelfDriving(boolean activated){
+        this.selfDriving = activated;
+        System.out.println("Self driving active: " + activated);
+    }
+
+    public boolean onSelfDrivingMode(){
+        return selfDriving;
+    }
+
+    @Override
+    public void honk(){
+        System.out.println("Tetttttt!");
+    }
+
+}
