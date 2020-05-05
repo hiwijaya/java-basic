6fda0025bb14aac6e7c8a164bde4b9dba209dcb6
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-05-05 10:55:10 +0700

    add project Lombok and learn lazy parameter trick

---
+++ src/main/java/com/hiwijaya/javabasic/LazyParameter.java
@@ -0,0 +1,80 @@
+package com.hiwijaya.javabasic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class LazyParameter {
+
+    private static ProductService productService = new ProductService();
+
+    // the problem is when we pass method in parameter, it will directly (eager) executed that method.
+//    public static Product findOrCreate(String id, Product newProduct){
+//        Product product = productService.findById(id);
+//        if(product != null){
+//            return product;
+//        }
+//        else{
+//            return newProduct;
+//        }
+//    }
+
+    // with this form, the method will only executed after get() is called
+    public static Product findOrCreate(String id, Supplier<Product> newProduct){
+        Product product = productService.findById(id);
+        if(product != null){
+            return product;
+        }
+        else{
+            return newProduct.get();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product product1 = findOrCreate("1", () -> productService.createNewProduct("1"));
+        System.out.println(product1);
+
+        Product product2 = findOrCreate("2", () -> productService.createNewProduct("2"));
+        System.out.println(product2);
+
+        Product product1Existed = findOrCreate("1", () -> productService.createNewProduct("1"));
+        System.out.println(product1Existed);
+
+    }
+
+}
+
+class ProductService {
+
+    private Map<String, Product> database = new HashMap<>();
+
+    public Product findById(String id){
+        return database.get(id);
+    }
+
+    public Product createNewProduct(String id){
+        System.out.println("Creating new product");
+        Product product = new Product(id, null);
+        database.put(id, product);
+        return product;
+    }
+
+}
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Product {
+
+    private String id;
+    private String name;
+
+}
+
+
