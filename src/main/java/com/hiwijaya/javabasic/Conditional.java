4779a8e264f92d6b8385c8866851dde316264822
Happy <happyindra@korindo.co.id>
2026-01-08 14:26:50 +0700

    feat: switch expression java 14

--- src/main/java/com/hiwijaya/javabasic/Conditional.java
+++ src/main/java/com/hiwijaya/javabasic/Conditional.java
@@ -23,7 +23,7 @@ public class Conditional {
 
     /*
     difference between if and switch is switch have 'domino effect'.
-    If it find true conditional once, it will fall-through(execute) the rest of case until it found 'break'.
+    If it finds true conditional once, it will fall-through(execute) the rest of case until it found 'break'.
      */
     public void switchControl(int n){
         switch (n){
@@ -43,6 +43,24 @@ public class Conditional {
         }
     }
 
+    /**
+     * Switch Expression (not just statement).
+     * Introduced in Java 14.
+     * {@code yield} keyword used to produce a value. Like {@code return}, but for switch block only.
+     */
+    public void updatedSwitchControl(int code){
+        String result = switch (code) {
+            case 200, 201 -> "OK";
+            case 404 -> "NOT FOUND";
+            case 500 -> {
+                System.out.println("Server error detected");
+                yield "INTERNAL SERVER ERROR";
+            }
+            default -> "UNKNOWN";
+        };
+        System.out.println(result);
+    }
+
     public void ternaryOperator(String name){
         String greeting = (name.equals("happy")) ? "Hello, Happy!" : "Who are you?";
         System.out.println(greeting);
@@ -51,9 +69,10 @@ public class Conditional {
 
     public static void main(String[] args) {
         Conditional c = new Conditional();
-        c.ifControl(1998);
-        c.switchControl(3);
-        c.ternaryOperator("happy");
+//        c.ifControl(1998);
+//        c.switchControl(3);
+        c.updatedSwitchControl(500);
+//        c.ternaryOperator("happy");
     }
 
 }
