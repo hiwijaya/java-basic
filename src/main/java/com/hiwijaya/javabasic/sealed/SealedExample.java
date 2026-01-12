4dc5c32f4bf240a9d14fbad69468ebaffe4c9990
Happy <happyindra@korindo.co.id>
2026-01-12 15:24:47 +0700

    feat: unused variables java 21

--- src/main/java/com/hiwijaya/javabasic/sealed/SealedExample.java
+++ src/main/java/com/hiwijaya/javabasic/sealed/SealedExample.java
@@ -5,9 +5,9 @@ public class SealedExample {
     static void main() {
         Payment payment = new Cash();
         String paidWith = switch (payment){
-            case Credit c -> "Credit Card";
-            case Card c -> "Card";
-            case Cash c -> "Cash";
+            case Credit _ -> "Credit Card";     // use _ for unused variables (Java 21)
+            case Card _ -> "Card";
+            case Cash _ -> "Cash";
         };
 
         System.out.println(paidWith);
